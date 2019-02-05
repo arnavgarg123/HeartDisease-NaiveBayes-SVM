14585b9e3453f8db9d3c02d1b39ec9a5d396f4e8
arnavgarg123 <arnav.garg9@gmail.com>
2019-02-05 15:27:37 +0530

    Data cleaning finished

--- classify.py
+++ classify.py
@@ -1,46 +1,40 @@
 import numpy as np
 import pandas as pd
 import seaborn as sns
-from sklearn.linear_model import LinearRegression
-linreg = LinearRegression()
+from sklearn.model_selection import train_test_split
+from sklearn import svm
 # importing the data from csv files
-# cd ~
-# cd Documents
-# cd GitHub
-# cd NaiveBayes - HeartDisease/
+model = svm.SVC(kernel='rbf', C=1, gamma=1)
 df1 = pd.read_csv(
-    '~/Documents/GitHub/NaiveBayes-HeartDisease/tubes2_HeartDisease_train.csv', na_values=['?'])
-
-
+    '~/WorkSpace//GitHub/NaiveBayes-HeartDisease/tubes2_HeartDisease_train.csv', na_values=['?'])
+#df1 = pd.read_csv('~/WorkSpace//GitHub/NaiveBayes-HeartDisease/heart.csv', na_values=['?'])
 df1.shape
-df1.dropna(thresh=8, inplace=True)
+#df1.dropna(thresh=8, inplace=True)
+df1.dropna(inplace=True)
+#
+X = df1[['Column1', 'Column2', 'Column3', 'Column4', 'Column5', 'Column6',
+         'Column7', 'Column8', 'Column9', 'Column10', 'Column11', 'Column12', 'Column13']]
+y = df1['Column14']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.4, random_state=101)
+model.fit(X_train, y_train)
+model.score(X_train, y_train)
+model.predict(X_test)
+# Plot
+sns.distplot(df1['Column14'])
+sns.heatmap(df1.corr())
+sns.pairplot(df1[['Column11', 'Column10']].dropna())
+#
 
-'''dfn=df.dropna()
-dfn.head()
-train_x=dfn.iloc[:,1:]
-train_y=dfn.iloc[:,:1]
-linreg.fit(train_x,train_y)
-test_data=df.iloc[:,1:]
-age_prediction['Column1']=pd.DataFrame(linreg.predict(test_data))
-'''
+df1.head(10)
 df1['Column5'].replace(0, 221, inplace=True)
 ##########        Data Cleaning        ##########
 # median to fill missing values
 df1['Column4'].fillna(df1['Column4'].median(), inplace=True)
-df1['Column5'].fillna(df1['Column4'].median(), inplace=True)
-df1['Column7'].fillna(df1['Column7'].max(), inplace=True)
-df1['Column8'].fillna(df1['Column8'].mean(), inplace=True)
-df1['Column9'].fillna(df1['Column9'].max(), inplace=True)
-df1['Column10'].fillna(df1['Column10'].mean(), inplace=True)
+df1['Column5'].fillna(df1['Column5'].median(), inplace=True)
+df1['Column6'].fillna(df1['Column6'].mode(), inplace=True)
+df1['Column7'].fillna(df1['Column7'].median(), inplace=True)
+df1['Column8'].fillna(df1['Column8'].median(), inplace=True)
+df1['Column9'].fillna(df1['Column9'].mode(), inplace=True)
+df1['Column10'].fillna(df1['Column10'].median(), inplace=True)
+df1[df1['Column11'].isnull()]
 df1.isnull().sum()
-dfn = df1.dropna()
-dfn.head()
-train_x = dfn.drop(['Column6'], axis=1).iloc[:18, [3, 4, 5, 8]]
-train_y = dfn.iloc[:18, 11]
-linreg.fit(train_x, train_y)
-test_data = df1.drop(['Column6'], axis=1).iloc[:18, [3, 4, 5, 8]]
-a = pd.DataFrame(linreg.predict(test_data))
-a.head(18)
-
-
-sns.pairplot(df1, hue='Column11')
